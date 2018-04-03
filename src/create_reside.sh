2ac7cfd976a751685e51bf6ca8930b7b31e07742
Guanlong Zhao <guanlongzhao@gmail.com>
2018-04-03 11:53:18 -0500

    update scripts

--- src/create_reside.sh
+++ src/create_reside.sh
@@ -6,12 +6,18 @@ OUTPUT=../toy_data
 DATA=../toy_data
 TOOLS=../../caffe/build/tools
 
-# Set RESIZE=true to resize the images to 256x256. Leave as false if images have
+# Remove previous run
+rm -r $OUTPUT/train/reside_train_clean_lmdb
+rm -r $OUTPUT/train/reside_train_haze_lmdb
+rm -r $OUTPUT/val/reside_val_clean_lmdb
+rm -r $OUTPUT/val/reside_val_haze_lmdb
+
+# Set RESIZE=true to resize the images. Leave as false if images have
 # already been resized using another tool.
-RESIZE=false
+RESIZE=true
 if $RESIZE; then
-  RESIZE_HEIGHT=256
-  RESIZE_WIDTH=256
+  RESIZE_HEIGHT=480
+  RESIZE_WIDTH=640
 else
   RESIZE_HEIGHT=0
   RESIZE_WIDTH=0
@@ -22,7 +28,6 @@ echo "Creating train haze lmdb..."
 GLOG_logtostderr=1 $TOOLS/convert_imageset \
     --resize_height=$RESIZE_HEIGHT \
     --resize_width=$RESIZE_WIDTH \
-    --shuffle \
     "" \
     $DATA/train_haze.txt \
     $OUTPUT/train/reside_train_haze_lmdb
@@ -32,7 +37,6 @@ echo "Creating train clean lmdb..."
 GLOG_logtostderr=1 $TOOLS/convert_imageset \
     --resize_height=$RESIZE_HEIGHT \
     --resize_width=$RESIZE_WIDTH \
-    --shuffle \
     "" \
     $DATA/train_clean.txt \
     $OUTPUT/train/reside_train_clean_lmdb
@@ -42,7 +46,6 @@ echo "Creating val haze lmdb..."
 GLOG_logtostderr=1 $TOOLS/convert_imageset \
     --resize_height=$RESIZE_HEIGHT \
     --resize_width=$RESIZE_WIDTH \
-    --shuffle \
     "" \
     $DATA/val_haze.txt \
     $OUTPUT/val/reside_val_haze_lmdb
@@ -52,7 +55,6 @@ echo "Creating val clean lmdb..."
 GLOG_logtostderr=1 $TOOLS/convert_imageset \
     --resize_height=$RESIZE_HEIGHT \
     --resize_width=$RESIZE_WIDTH \
-    --shuffle \
     "" \
     $DATA/val_clean.txt \
     $OUTPUT/val/reside_val_clean_lmdb
