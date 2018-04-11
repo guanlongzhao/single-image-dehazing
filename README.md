9994550e986e36bfbda4b3554b18fb7438e919ca
Guanlong Zhao <guanlongzhao@gmail.com>
2018-04-10 20:22:21 -0500

    modify script to be able to generate any data split

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # single-image-dehazing
 633 course project.
+
+Total number of images that can be used for training: 397636
--- src/create_data_list.py
+++ src/create_data_list.py
@@ -1,23 +1,48 @@
 #!/bin/bash
+# valid options
+#   -t: num of training samples, default to 1000
+#   -v: num of val samples, default to 1000
+# Example: python create_data_list.py -t 10000 -v 1000
+# 
+# This script will first find all possible haze images, and then shuffle their order
+# and then pick the first val_size files as the val set, and then pick the following
+# train_size files as the training set
+# The resulting list for training and val will reflect the distribution of OTS
+# and ITS in RESIDE, so we do not have equal number of images from two groups
+
 from glob import glob
 import os
 import math
+import random
+from sys import argv
+
+def getopts(argv):
+    opts = {}  # Empty dictionary to store key-value pairs.
+    while argv:  # While there are arguments left to parse...
+        if argv[0][0] == '-':  # Found a "-name value" pair.
+            opts[argv[0]] = argv[1]  # Add key and value to the dictionary.
+        argv = argv[1:]  # Reduce the argument list by copying it starting from index 1.
+    return opts
 
 def getCleanName(in_name):
     return in_name.split('.')[0].split('_')[0]
 
-# global settings
-isToyData = True
+## get input params
+myargs = getopts(argv)
+train_size = 1000
+val_size = 1000
+if '-t' in myargs:
+    train_size = int(myargs['-t'])
+if '-v' in myargs:
+    val_size = int(myargs['-v'])
+print 'Num train imgs: %s\nNum val imgs: %d' % (train_size, val_size)
+
+## global settings
 fake_label = 0 # we do not care the label's value
 its_val_start = 10001 # its val set starts from 10001.png
-if isToyData:
-    toy_train_data_size = 1000
-    toy_val_data_size = 1000
-    toy_data_size = toy_train_data_size + toy_val_data_size
-else:
-    ots_train_portion = 0.9
+data_post_fix = '%dk' % (round(train_size/1000))
 
-# setup dirs
+## setup dirs
 raw_data_root = '/media/guanlong/DATA/633_data/'
 its_train_haze = raw_data_root + 'ITS/train/ITS_haze/'
 its_train_clean = raw_data_root + 'ITS/train/ITS_clear/'
@@ -25,92 +50,83 @@ its_val_haze = raw_data_root + 'ITS/val/haze/'
 its_val_clean = raw_data_root + 'ITS/val/clear/'
 ots_haze = raw_data_root + 'OTS_haze/'
 ots_clean = raw_data_root + 'OTS_clean/'
-if isToyData:
-    data_tag = 'toy_'
-else:
-    data_tag = ''
-data_train = '../%sdata/train/' % (data_tag)
-data_val = '../%sdata/val/' % (data_tag)
+data_root = '../data%s' % (data_post_fix)
+data_train = '%s/train/' % (data_root)
+data_val = '%s/val/' % (data_root)
+if not os.path.exists(data_root):
+    os.mkdir(data_root)
 if not os.path.exists(data_train):
     os.mkdir(data_train)
 if not os.path.exists(data_val):
     os.mkdir(data_val)
-train_haze_txt = open('../%sdata/train_haze.txt' % (data_tag), 'w')
-train_clean_txt = open('../%sdata/train_clean.txt' % (data_tag), 'w')
-val_haze_txt = open('../%sdata/val_haze.txt' % (data_tag), 'w')
-val_clean_txt = open('../%sdata/val_clean.txt' % (data_tag), 'w')
+train_haze_txt = open('%s/train_haze.txt' % (data_root), 'w')
+train_clean_txt = open('%s/train_clean.txt' % (data_root), 'w')
+val_haze_txt = open('%s/val_haze.txt' % (data_root), 'w')
+val_clean_txt = open('%s/val_clean.txt' % (data_root), 'w')
 
-# get full file list
+## get full file list
 its_haze_list = (glob(its_train_haze + '*.png') + glob(its_val_haze + '*.png'))
 its_haze_list.sort()
-its_clean_list = glob(its_train_clean + '*.png') + glob(its_val_clean + '*.png')
-for ii in range(len(its_clean_list)):
-    its_clean_list[ii] = its_clean_list[ii].split('/')[-1]
-its_clean_list = set(its_clean_list)
+its_clean_list = [] # get the clean file for each haze image
+for haze_file in its_haze_list:
+    haze_name = haze_file.split('/')[-1]
+    clean_idx = getCleanName(haze_name)
+    clean_idx_num = int(clean_idx)
+    clean_name = clean_idx + '.png'
+    isInTrainSet = (clean_idx_num < its_val_start)
+    if isInTrainSet: # some one did a stupid data split
+        clean_file = '%s%s' % (its_train_clean, clean_name)
+    else:
+        clean_file = '%s%s' % (its_train_clean, clean_name)
+    its_clean_list.append(clean_file)
 
 ots_haze_list = glob(ots_haze + '*.jpg')
 ots_haze_list.sort()
-ots_clean_list = glob(ots_clean + '*.jpg')
-for ii in range(len(ots_clean_list)):
-    ots_clean_list[ii] = ots_clean_list[ii].split('/')[-1]
-ots_clean_list = set(ots_clean_list)
+ots_clean_list = [] # get the clean file for each haze image
+for haze_file in ots_haze_list:
+    haze_name = haze_file.split('/')[-1]
+    clean_idx = getCleanName(haze_name)
+    clean_name = clean_idx + '.jpg'
+    clean_file = '%s%s' % (ots_clean, clean_name)
+    ots_clean_list.append(clean_file)
 
-# build the txt files
-if isToyData: # only use OTS
-    for ii in range(toy_data_size):
-        haze_name = ots_haze_list[ii].split('/')[-1]
-        clean_name = getCleanName(haze_name) + '.jpg'
-        isCleanExists = (clean_name in ots_clean_list)
-        haze_output = '%s%s %d\n' % (ots_haze, haze_name, fake_label)
-        clean_output = '%s%s %d\n' % (ots_clean, clean_name, fake_label)
-        if isCleanExists:
-            if ii < toy_val_data_size:
-                train_haze_txt.write(haze_output)
-                train_clean_txt.write(clean_output)
-            else:
-                val_haze_txt.write(haze_output)
-                val_clean_txt.write(clean_output)
-else: # real data
-    for haze_name in its_haze_list: # deal with ITS, png
-        haze_name = haze_name.split('/')[-1]
-        clean_idx = getCleanName(haze_name)
-        clean_idx_num = int(clean_idx)
-        clean_name = clean_idx + '.png'
-        isCleanExists = (clean_name in its_clean_list)
-        isTrainingSample = (clean_idx_num < its_val_start)
-        if isTrainingSample: # in training set
-            haze_output = '%s%s %d\n' % (its_train_haze, haze_name, fake_label)
-            clean_output = '%s%s %d\n' % (its_train_clean, clean_name, fake_label)
-        else: # in val set
-            haze_output = '%s%s %d\n' % (its_val_haze, haze_name, fake_label)
-            clean_output = '%s%s %d\n' % (its_val_clean, clean_name, fake_label)
-        if isCleanExists:
-            if isTrainingSample:
-                train_haze_txt.write(haze_output)
-                train_clean_txt.write(clean_output)
-            else:
-                val_haze_txt.write(haze_output)
-                val_clean_txt.write(clean_output)
+haze_list = its_haze_list + ots_haze_list
+clean_list = its_clean_list + ots_clean_list
+# print 'Num haze image: %d\nNum clean image: %d' % (len(haze_list), len(clean_list))
+assert len(haze_list) == len(clean_list)
 
-    # determine the data split for OTS set
-    ots_val_start = math.ceil(len(ots_haze_list)*ots_train_portion) + 1
-    nSample = 0
-    for haze_name in ots_haze_list: # deal with OTS, jpg
-        haze_name = haze_name.split('/')[-1]
-        nSample += 1
-        clean_idx = getCleanName(haze_name)
-        clean_name = clean_idx + '.jpg'
-        isCleanExists = (clean_name in ots_clean_list)
-        isTrainingSample = (nSample < ots_val_start)
-        haze_output = '%s%s %d\n' % (ots_haze, haze_name, fake_label)
-        clean_output = '%s%s %d\n' % (ots_clean, clean_name, fake_label)
-        if isCleanExists:
-            if isTrainingSample: # in train set
-                train_haze_txt.write(haze_output)
-                train_clean_txt.write(clean_output)
-            else: # in val set
-                val_haze_txt.write(haze_output)
-                val_clean_txt.write(clean_output)
+# shuffle the order of files
+combined = list(zip(haze_list, clean_list))
+random.shuffle(combined)
+haze_list[:], clean_list[:] = zip(*combined)
+numImgs = len(haze_list)
+
+## build the txt files
+# first fulfill val requirement
+if numImgs > val_size: # enough for val, not equal to ensure at least one training
+    val_list = range(0, val_size)
+    if (numImgs - val_size) >= train_size: # enough for training
+        train_list = range(val_size, (val_size+train_size))
+    else: # not enough for train, use the rest
+        train_list = range(val_size, numImgs)
+        print 'No enough OTS samples for training, actual training set is %d images' %(len(train_list))
+else: # not enough for val
+    print 'You do not need such a big val set from ITS, go pick a smaller one!'
+    exit()
+
+# get the txt files
+for ii in val_list:
+    haze_output = '%s %d\n' % (haze_list[ii], fake_label)
+    val_haze_txt.write(haze_output)
+    clean_output = '%s %d\n' % (clean_list[ii], fake_label)
+    val_clean_txt.write(clean_output)
+
+for jj in train_list:
+    haze_output = '%s %d\n' % (haze_list[jj], fake_label)
+    train_haze_txt.write(haze_output)
+    clean_output = '%s %d\n' % (clean_list[jj], fake_label)
+    train_clean_txt.write(clean_output)
+
 train_haze_txt.close()
 train_clean_txt.close()
 val_haze_txt.close()
